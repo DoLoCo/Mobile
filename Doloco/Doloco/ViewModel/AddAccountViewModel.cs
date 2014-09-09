54891e1dec037d940f041acabf20490494e69b69
billpull <bill.pull@gmail.com>
2014-09-08 21:36:15 -0400

    rebase this

--- Doloco/Doloco/ViewModel/AddAccountViewModel.cs
+++ Doloco/Doloco/ViewModel/AddAccountViewModel.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
 using System.Diagnostics;
 using System.Threading.Tasks;
 using Xamarin.Forms;
@@ -9,56 +11,74 @@ namespace Doloco
 	public class AddAccountViewModel:BaseViewModel
 	{
 		private readonly INavigation _navigation;
+	    private IDictionary<string, string> _accountTypes;
+
 		public AddAccountViewModel(INavigation navigation)
 		{
-			this._navigation = navigation;
+			_navigation = navigation;
+            _accountTypes = new Dictionary<string, string>()
+            {
+                {"0", "Checking"},
+                {"1", "savings"}
+            };
 		}
 
 		public const string AccountTypePropertyName = "AccountType";
-		private string accountType = string.Empty;
+		private string _accountType = string.Empty;
 		public string AccountType
 		{
-			get { return accountType; }
-			set { SetProperty(ref accountType, value, AccountTypePropertyName); }
+			get { return _accountType; }
+			set { SetProperty(ref _accountType, value, AccountTypePropertyName); }
 		}
 
 		public const string AccountNamePropertyName = "AccountName";
-		private string accountName = string.Empty;
+		private string _accountName = string.Empty;
 		public string AccountName
 		{
-			get { return accountName; }
-			set { SetProperty(ref accountName, value, AccountNamePropertyName); }
+			get { return _accountName; }
+			set { SetProperty(ref _accountName, value, AccountNamePropertyName); }
 		}
 
 		public const string AccountNumberPropertyName = "AccountNumber";
-		private string accountNumber = string.Empty;
+		private string _accountNumber = string.Empty;
 		public string AccountNumber
 		{
-			get { return accountNumber; }
-			set { SetProperty(ref accountNumber, value, AccountNumberPropertyName); }
+			get { return _accountNumber; }
+			set { SetProperty(ref _accountNumber, value, AccountNumberPropertyName); }
 		}
 
 		public const string RoutingNumberPropertyName = "RoutingNumber";
-		private string routingNumber = string.Empty;
+		private string _routingNumber = string.Empty;
 		public string RoutingNumber
 		{
-			get { return routingNumber; }
-			set { SetProperty(ref routingNumber, value, RoutingNumberPropertyName); }
+			get { return _routingNumber; }
+			set { SetProperty(ref _routingNumber, value, RoutingNumberPropertyName); }
 		}
 
-		private Command addCommand;
+		private Command _addCommand;
 		public const string AddCommandPropertyName = "AddCommand";
 		public Command AddCommand
 		{
 			get
 			{
-				return addCommand ?? (addCommand = new Command(async () => await ExecuteAddCommand()));
+				return _addCommand ?? (_addCommand = new Command(async () => await ExecuteAddCommand()));
 			}
 		}
 
 		protected async Task ExecuteAddCommand()
 		{
-			Debug.WriteLine(accountName);
+		    string accountType; 
+            _accountTypes.TryGetValue(_accountType, out accountType);
+
+		    try
+		    {
+		        await App.ApiClient.CreateBankAccountAsync(_accountNumber, accountType, _routingNumber, _accountName);
+		    }
+		    catch (Exception ex)
+		    {
+                var page = new ContentPage();
+                var result = page.DisplayAlert("Error", ex.Message, "OK", "Cancel");
+		    }
 		}
 	}
 }
